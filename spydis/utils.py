64e4aba1403a489e2ab44e000026a83e29a4c250
tuyenmassey@gmail.com <tuyenmassey@gmail.com>
2022-12-03 20:46:19 +0100

    Initial commit

--- spydis/utils.py
+++ spydis/utils.py
@@ -3,6 +3,7 @@
 import xarray as xr
 import rioxarray as rx
 from dateutil.relativedelta import relativedelta
+import datetime as dt
 
 def time_dimension(ds, start_date=None, freq=None,input_dim=None,out_dim=None):
     """ A helper function to add a time dimension to the DataArray.
@@ -24,7 +25,8 @@ def time_dimension(ds, start_date=None, freq=None,input_dim=None,out_dim=None):
     if start_date is None:
         start_date=dt.datetime.strptime("2000-02-01","%Y-%m-%d")
     else:
-        start_date=dt.datetime.strptime(f"{start_date}","%Y-%m-%d")
+        if check_date(start_date):
+            start_date=dt.datetime.strptime(f"{start_date}","%Y-%m-%d")
     if input_dim is None:
         input_dim="band"
     else:
@@ -40,11 +42,24 @@ def time_dimension(ds, start_date=None, freq=None,input_dim=None,out_dim=None):
         if freq.lower() in ["d","day","days"]:
             start_date=[start_date+relativedelta(days=i) for i in range(len(ds))]
         elif freq.lower() in ["y","year","years"]:
-
             start_date=[start_date+relativedelta(years=i) for i in range(len(ds))]
     if not input_dim.lower() in ds.dims:
         print(f"{input_dim} is not one of the dimensions in the DataArray")
-        raise ValueError("Please check the desired dimension that wanted to convert to time dimension.")
+        raise ValueError("Please check the desired dimension that wanted to convert into time dimension.")
     ds[input_dim]=start_date
     ds=ds.rename({input_dim:out_dim})
     return ds
+
+def check_date(date_str):
+    """ Check if date is in a required format.
+
+        Args:
+            date_str (str): Date in string format.
+    """
+    fmts="%Y-%m-%d"
+    try:
+        _date=dt.datetime.strptime(date_str,fmts)
+        return True
+    except Exception:
+        raise Exception
+
